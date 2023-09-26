55f0acb6581b0813501c5ae98052f555c56e8ed3
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-26 17:50:54 +0200

    feat: update function

--- app/Http/Controllers/Api/PlateController.php
+++ app/Http/Controllers/Api/PlateController.php
@@ -39,6 +39,24 @@ class PlateController extends Controller
         return response()->json();
     }
 
+    public function update(Request $request, $id)
+    {
+        $plate = Plate::find($id);
+
+        if (!$plate) return response(null, 404);
+
+        $data = $request->all();
+
+        $plate->name = $data['name'];
+        $plate->price = $data['price'];
+        $plate->image = $data['image'];
+        $plate->ingredients = $data['ingredients'];
+        $plate->description = $data['description'];
+        $plate->save();
+
+        return response()->json('Updated successfully');
+    }
+
     public function destroy(string $id)
     {
         $plate = Plate::find($id);
