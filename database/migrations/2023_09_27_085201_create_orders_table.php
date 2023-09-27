b429f990607b142c6e9772d316364f281cf868e7
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-09-27 16:59:55 +0200

    feat: migration for table orders

---
+++ database/migrations/2023_09_27_085201_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->date('order_date');
+            $table->string('name');
+            $table->string('surname');
+            $table->boolean('status')->nullable();
+            $table->string('address');
+            $table->text('note')->nullable();
+            $table->float('total_price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
