49cbe11036272e0fe82bfe9bc1f0983346f2b2f2
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-27 17:44:51 +0200

    feat: config file with plates

---
+++ config/plates.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    [
+        "name" => "Pizza Margherita",
+        "price" => 10.99,
+        "image" => null,
+        "ingredients" => "Pomodoro, mozzarella, basilico",
+        "description" => "La classica pizza Margherita con pomodoro fresco, mozzarella di bufala e basilico."
+    ],
+    [
+        "name" => "Pasta alla Carbonara",
+        "price" => 12.99,
+        "image" => "https://www.identitagolose.it/public/images/xmedium/pizza-2.jpg",
+        "ingredients" => "Spaghetti, uova, pancetta, pecorino",
+        "description" => "Una deliziosa pasta alla carbonara con spaghetti cotti al dente, uova fresche, pancetta croccante e pecorino romano."
+    ],
+    [
+        "name" => "Sushi Misto",
+        "price" => 18.99,
+        "image" => null,
+        "ingredients" => "Riso, pesce crudo, alga nori, avocado",
+        "description" => "Un assortimento di sushi fresco con pesce crudo, avocado e riso appena preparato."
+    ],
+    [
+        "name" => "Bistecca alla Griglia",
+        "price" => 24.99,
+        "image" => "https://www.identitagolose.it/public/images/xmedium/pizza-2.jpg",
+        "ingredients" => "Bistecca di manzo, sale, pepe, olio d'oliva",
+        "description" => "Una succulenta bistecca alla griglia servita con sale, pepe e olio d'oliva."
+    ],
+    [
+        "name" => "Insalata Caprese",
+        "price" => 8.99,
+        "image" => null,
+        "ingredients" => "Pomodoro, mozzarella di bufala, basilico, olio d'oliva",
+        "description" => "Un'insalata fresca e leggera con pomodoro, mozzarella di bufala, basilico e olio d'oliva extravergine."
+    ]
+];
