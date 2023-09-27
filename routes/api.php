5c2a3c64ccecaf2ed60302cecae73be1d0512a9f
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-27 15:05:16 +0200

    feat: routes for softdeletes and trash

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/plates/trash', [PlateController::class, 'trash']);
+
+Route::delete('/plates/trash/{id}/drop', [PlateController::class, 'drop']);
+
+Route::delete('/plates/trash/drop', [PlateController::class, 'dropAll']);
+
 Route::apiResource('/plates', PlateController::class);
