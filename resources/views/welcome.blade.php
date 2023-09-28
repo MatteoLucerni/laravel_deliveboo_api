7c3538cf50c3730aa3c8fd967fab5bac02584615
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-28 17:16:40 +0200

    fix: same layouts in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,15 @@
             <div class="logo_laravel">
 
             </div>
-            <h1 class="display-5 fw-bold">
+            <h1 class="display-5 fw-bold text-center">
                 Welcome to Deliveboo
             </h1>
-            <p class="fw-bold py-2">Progetto : Matteo Lucerni , Guglielmo Piasenti , Matteo Nesti , Giacomo Arcangeli ,
+            <p class="fw-bold py-2 text-center">Progetto : Matteo Lucerni , Guglielmo Piasenti , Matteo Nesti , Giacomo
+                Arcangeli ,
                 Andrea
                 Creazzi</p>
-            <a href="{{ route('admin.plates.index') }}" class="btn btn-primary btn-lg">Entra nel magico mondo dei
+            <a href="{{ route('admin.plates.index') }}" class="btn btn-primary btn-lg d-flex justify-content-center">Entra nel
+                magico mondo dei
                 ristoratori</a>
         </div>
     </div>
