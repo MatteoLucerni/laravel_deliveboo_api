47d2e47a15f96ef3839a37d406c9e1e98f18c006
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-28 20:07:07 +0200

    feat: added links for different pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,23 @@
             <h1 class="display-5 fw-bold text-center">
                 Welcome to Deliveboo
             </h1>
-            <p class="fw-bold py-2 text-center">Progetto : Matteo Lucerni , Guglielmo Piasenti , Matteo Nesti , Giacomo
-                Arcangeli ,
+            <p class="py-2 text-center">Matteo Lucerni, Guglielmo Piasenti, Matteo Nesti, Giacomo
+                Arcangeli,
                 Andrea
                 Creazzi</p>
-            <a href="{{ route('admin.plates.index') }}" class="btn btn-primary btn-lg d-flex justify-content-center">Entra nel
-                magico mondo dei
-                ristoratori</a>
+            @auth
+                <div class="row gap-3 justify-content-center ">
+                    <a class="col-5 h-100 btn btn-primary btn-lg d-flex justify-content-center"
+                        href="{{ route('admin.plates.index') }}">Manage your restaurant and menù</a>
+                    <a class="col-5 h-100 btn btn-success btn-lg d-flex justify-content-center"
+                        href="{{ route('admin.orders.index') }}">Manage your restaurant's orders</a>
+                </div>
+            @else
+                <a href="{{ route('admin.plates.index') }}" class="btn btn-primary btn-lg d-flex justify-content-center">Entra
+                    nel
+                    magico mondo dei
+                    ristoratori</a>
+            @endauth
         </div>
     </div>
 @endsection
