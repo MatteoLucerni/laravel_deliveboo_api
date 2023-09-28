0d6b5f581dfc62d51f728d3fb9c4597fdf1e09d4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-28 17:42:35 +0200

    using include to unify

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -15,51 +15,10 @@
         <div class="container">
             <h3>Add a plate</h3>
 
-            <form action="{{ route('admin.plates.store') }}" class="needs-validation" method="POST" novalidate>
-                @csrf
-                <!-- Plate Name -->
-                <div class="mb-3">
-                    <label for="plateName" class="form-label">Plate Name</label>
-                    <input name="name" v-model.trim="plate.name" type="text" class="form-control" id="plateName"
-                        placeholder="Enter Plate Name" required>
-                </div>
+            @include('components.form', ['plate' => new \App\Models\Plate()])
 
-                <!-- Plate Image -->
-                <div class="mb-3">
-                    <label for="plateImage" class="form-label">Plate Image URL</label>
-                    <input name="image" v-model.trim="plate.image" type="url" class="form-control" id="plateImage"
-                        placeholder="Enter Plate Image URL" required>
-                </div>
-
-                <!-- Plate Price -->
-                <div class="mb-3">
-                    <label for="platePrice" class="form-label">Plate Price</label>
-                    <input name="price" v-model.trim="plate.price" type="number" class="form-control" id="platePrice"
-                        placeholder="Enter Plate Price" required>
-                </div>
-
-                <!-- Plate Ingredients -->
-                <div class="mb-3">
-                    <label for="plateIngredients" class="form-label">Plate Ingredients</label>
-                    <textarea name="ingredients" v-model.trim="plate.ingredients" class="form-control" id="plateIngredients"
-                        placeholder="Enter Plate Ingredients" required></textarea>
-                </div>
-
-                <!-- Plate Description -->
-                <div class="mb-3">
-                    <label for="plateDescription" class="form-label">Plate Description</label>
-                    <textarea name="description" v-model.trim="plate.description" class="form-control" id="plateDescription"
-                        placeholder="Enter Plate Description" required></textarea>
-                </div>
-
-                <!-- Plate Description -->
-                <div class="mb-3">
-                    <label for="is_visible" class="form-label">Visible?</label>
-                    <input value="1" type="checkbox" name="is_visible" id="is_visible">
-                </div>
-
-                <!-- Submit Button -->
-                <button type="submit" class="btn btn-success">Submit</button>
+            <!-- Submit Button -->
+            <button type="submit" class="btn btn-success">Submit</button>
             </form>
         </div>
     </div>
