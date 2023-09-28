9a5dfe4d2a1175ad5aa2832bb0712ebfea1290e7
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-28 17:13:25 +0200

    fix: layout home + add button to move in plate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,21 +6,13 @@
 
             </div>
             <h1 class="display-5 fw-bold">
-                Welcome to Laravel+Bootstrap 5
+                Welcome to Deliveboo
             </h1>
-
-            <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel
-                breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
-            <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg"
-                type="button">Documentation</a>
-        </div>
-    </div>
-
-    <div class="content">
-        <div class="container">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
-                deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
-                accusamus dolores!</p>
+            <p class="fw-bold py-2">Progetto : Matteo Lucerni , Guglielmo Piasenti , Matteo Nesti , Giacomo Arcangeli ,
+                Andrea
+                Creazzi</p>
+            <a href="{{ route('admin.plates.index') }}" class="btn btn-primary btn-lg">Entra nel magico mondo dei
+                ristoratori</a>
         </div>
     </div>
 @endsection
