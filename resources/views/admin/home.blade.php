b500ea9b52837f0c5f7017fba2071599012de0db
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-28 12:42:02 +0200

    fix: renamed dashboard in home

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+        <div class="container py-5">
+            <div class="logo_laravel">
+
+            </div>
+            <h1 class="display-5 fw-bold">
+                Admin page
+            </h1>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
+                deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
+                accusamus dolores!</p>
+        </div>
+    </div>
+@endsection
