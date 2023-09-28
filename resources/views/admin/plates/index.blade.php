75fd9d5e5ad9486f217392b4043d14da23855d5b
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-28 12:42:47 +0200

    feat: added plates main page

---
+++ resources/views/admin/plates/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+        <div class="container py-5">
+            <div class="logo_laravel">
+
+            </div>
+            <h1 class="display-5 fw-bold">
+                Plates page
+            </h1>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+            <h3>{{ $restaurant->name }}</h3>
+
+            @forelse ($plates as $plate)
+                <li class="my-3">
+                    {{ $plate->name }}
+                    <a href="{{ route('admin.plates.show', $plate->id) }}" class="btn btn-primary">Details</a>
+                </li>
+            @empty
+                <h3>No plates</h3>
+            @endforelse
+        </div>
+    </div>
+@endsection
