8e7a450006abe39d44931e4ece1e7ef6705101ee
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-29 13:15:01 +0200

    feat: added orders index return

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        $orders = []; // TODO
+        $orders = Order::orderBy('created_at', 'ASC')->get();
         return view('admin.orders.index', compact('orders'));
     }
 }
