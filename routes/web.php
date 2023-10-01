c7a69d7c4bcac451f0f9e8dce15d022945b74f24
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-01 18:35:13 +0200

    feat: trash in order controller and route in web

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\RestaurantController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Restaurant;
+use App\Models\Order;
 use App\Models\Plate;
 use Illuminate\Support\Facades\Route;
 
@@ -33,17 +34,23 @@ Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function ()
     Route::delete('/plates/trash/{id}/drop', [PlateController::class, 'drop'])->name('plates.drop');
     Route::delete('/plates/trash/drop', [PlateController::class, 'dropAll'])->name('plates.dropAll');
     Route::patch('/plates/{id}/restore', [PlateController::class, 'restore'])->name('plates.restore');
-
     Route::resource('/plates', PlateController::class);
+    // end routes for plates
 
     // routes for resturants
     Route::patch('/restaurants/{id}', [RestaurantController::class, 'update'])->name('restaurants.update');
     Route::get('/restaurants/{id}/edit', [RestaurantController::class, 'edit'])->name('restaurants.edit');
+    // end routes for restaurants
 
     // routes for orders
+    Route::get('/orders/trash', [OrderController::class, 'trash'])->name('orders.trash');
+    Route::delete('/orders/trash/{id}/drop', [OrderController::class, 'drop'])->name('orders.drop');
+    Route::delete('/orders/trash/drop', [OrderController::class, 'dropAll'])->name('orders.dropAll');
+    Route::patch('/orders/{id}/restore', [OrderController::class, 'restore'])->name('orders.restore');
     Route::resource('/orders', OrderController::class);
 });
 
+// routes for profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
