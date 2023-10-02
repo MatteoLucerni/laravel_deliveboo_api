79ebd887d14c3bd6dfcfbfbd8d8fe1c09fcb9ef3
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-02 11:25:41 +0200

    fix: changed message language

--- resources/js/delete-confirm.js
+++ resources/js/delete-confirm.js
@@ -4,9 +4,7 @@ deleteForm.forEach((form) => {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         const orderName = form.dataset.name;
-        const isConfirmed = confirm(
-            `Sei sicuro di voler eliminare ${orderName}`
-        );
+        const isConfirmed = confirm(`Are you sure deleting ${orderName}?`);
         if (isConfirmed) form.submit();
     });
 });
