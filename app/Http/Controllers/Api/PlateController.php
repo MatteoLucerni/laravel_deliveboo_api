ca0232ba75ce3d3d701497fd918656b4edd20a88
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-02 12:07:05 +0200

    fix: commented crud for plates

--- app/Http/Controllers/Api/PlateController.php
+++ app/Http/Controllers/Api/PlateController.php
@@ -25,113 +25,113 @@ class PlateController extends Controller
         return response()->json($plate);
     }
 
-    public function store(Request $request)
-    {
-        $data = $request->all();
+    // public function store(Request $request)
+    // {
+    //     $data = $request->all();
 
-        $validator = Validator::make($data, [
-            'name' => 'required|string',
-            'price' => 'required|numeric',
-            'image' => 'nullable|string',
-            'ingredients' => 'required|string',
-            'description' => 'nullable|string'
-        ]);
+    //     $validator = Validator::make($data, [
+    //         'name' => 'required|string',
+    //         'price' => 'required|numeric',
+    //         'image' => 'nullable|string',
+    //         'ingredients' => 'required|string',
+    //         'description' => 'nullable|string'
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json(['errors' => $validator->errors()], 400);
+    //     }
 
-        $plate = new Plate();
-        $plate->name = $data['name'];
-        $plate->price = $data['price'];
-        $plate->image = $data['image'];
-        $plate->ingredients = $data['ingredients'];
-        $plate->description = $data['description'];
-        $plate->save();
+    //     $plate = new Plate();
+    //     $plate->name = $data['name'];
+    //     $plate->price = $data['price'];
+    //     $plate->image = $data['image'];
+    //     $plate->ingredients = $data['ingredients'];
+    //     $plate->description = $data['description'];
+    //     $plate->save();
 
-        return response()->json($plate);
-    }
+    //     return response()->json($plate);
+    // }
 
-    public function update(Request $request, $id)
-    {
-        $plate = Plate::find($id);
+    // public function update(Request $request, $id)
+    // {
+    //     $plate = Plate::find($id);
 
-        if (!$plate) return response(null, 404);
+    //     if (!$plate) return response(null, 404);
 
-        $data = $request->all();
+    //     $data = $request->all();
 
-        $validator = Validator::make($data, [
-            'name' => 'required|string',
-            'price' => 'required|numeric',
-            'image' => 'nullable|string',
-            'ingredients' => 'required|string',
-            'description' => 'nullable|string'
-        ]);
+    //     $validator = Validator::make($data, [
+    //         'name' => 'required|string',
+    //         'price' => 'required|numeric',
+    //         'image' => 'nullable|string',
+    //         'ingredients' => 'required|string',
+    //         'description' => 'nullable|string'
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json(['errors' => $validator->errors()], 400);
+    //     }
 
-        $plate->name = $data['name'];
-        $plate->price = $data['price'];
-        $plate->image = $data['image'];
-        $plate->ingredients = $data['ingredients'];
-        $plate->description = $data['description'];
-        $plate->save();
+    //     $plate->name = $data['name'];
+    //     $plate->price = $data['price'];
+    //     $plate->image = $data['image'];
+    //     $plate->ingredients = $data['ingredients'];
+    //     $plate->description = $data['description'];
+    //     $plate->save();
 
-        return response()->json('Updated successfully');
-    }
+    //     return response()->json('Updated successfully');
+    // }
 
-    public function trash()
-    {
-        $plates = Plate::onlyTrashed()->get();
-        return response()->json($plates);
-    }
+    // public function trash()
+    // {
+    //     $plates = Plate::onlyTrashed()->get();
+    //     return response()->json($plates);
+    // }
 
-    public function destroy(string $id)
-    {
-        $plate = Plate::find($id);
+    // public function destroy(string $id)
+    // {
+    //     $plate = Plate::find($id);
 
-        if (!$plate) return response(null, 404);
+    //     if (!$plate) return response(null, 404);
 
-        $plate->delete();
+    //     $plate->delete();
 
-        return response()->json('Plate moved to trash successfully');
-    }
+    //     return response()->json('Plate moved to trash successfully');
+    // }
 
-    public function dropAll()
-    {
-        Plate::onlyTrashed()->forceDelete();
+    // public function dropAll()
+    // {
+    //     Plate::onlyTrashed()->forceDelete();
 
-        return response()->json('Deleted all successfully');
-    }
+    //     return response()->json('Deleted all successfully');
+    // }
 
-    public function drop(string $id)
-    {
-        $plate = Plate::onlyTrashed()->find($id);
+    // public function drop(string $id)
+    // {
+    //     $plate = Plate::onlyTrashed()->find($id);
 
-        if (!$plate) return response(null, 404);
+    //     if (!$plate) return response(null, 404);
 
-        $plate->forceDelete();
+    //     $plate->forceDelete();
 
-        return response()->json('Plate deleted successfully');
-    }
+    //     return response()->json('Plate deleted successfully');
+    // }
 
-    public function restore(string $id)
-    {
-        $plate = Plate::onlyTrashed()->find($id);
+    // public function restore(string $id)
+    // {
+    //     $plate = Plate::onlyTrashed()->find($id);
 
-        if (!$plate) return response(null, 404);
+    //     if (!$plate) return response(null, 404);
 
-        $plate->restore();
+    //     $plate->restore();
 
-        return response()->json('Plate restored successfully');
-    }
+    //     return response()->json('Plate restored successfully');
+    // }
 
-    public function restoreAll()
-    {
-        Plate::onlyTrashed()->restore();
+    // public function restoreAll()
+    // {
+    //     Plate::onlyTrashed()->restore();
 
-        return response()->json('All plates restored successfully');
-    }
+    //     return response()->json('All plates restored successfully');
+    // }
 }
