f8df4837307486281443af7f256e18bfd4bd1194
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-02 12:07:14 +0200

    feat: added index and show

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index()
+    {
+        $restaurants = Restaurant::whereNull('deleted_at')->get();
+
+        return response()->json(compact('restaurants'));
+    }
+
+    public function show(string $id)
+    {
+        $restaurant = Restaurant::find($id);
+
+        if (!$restaurant) return response(null, 404);
+
+        return response()->json($restaurant);
+    }
+}
