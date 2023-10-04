12c26c5e5a63e4204fb131b776b84067e8fc1863
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-10-04 14:53:32 +0200

    feat : validate for edit restaurant form

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -39,14 +39,19 @@ class RestaurantController extends Controller
         $request->validate([
             'name' => 'required|string',
             'address' => 'required|string',
+            'phone_number' => 'required|size:10',
+            'description' => 'nullable|string',
             'image' => 'nullable|url',
             'vat_number' => 'required|size:13',
             'types' => 'required|exists:types,id',
         ], [
             'name.required' => 'Name field is required',
-            'name.string' => 'Name field must me a string',
+            'name.string' => 'Name field must be a string',
             'address.required' => 'Address field is required',
             'address.string' => 'Address field must me a string',
+            'phone_number.required' => 'Phone number field is required',
+            'phone_number.size' => 'Phone number must contain 13 charaters',
+            'description.string' => 'Name field must be a string',
             'vat_number.required' => 'Vat number field is required',
             'vat_number.size' => 'Vat number must contain 13 charaters',
             'types.required' => 'At least a type is required'
