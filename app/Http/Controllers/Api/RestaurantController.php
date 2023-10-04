afee2bb1f432ef938c636d68851c6190709b120b
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-04 19:04:57 +0200

    fix: pagination number

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -28,7 +28,7 @@ class RestaurantController extends Controller
             $restaurants->where('name', 'LIKE', '%' . $keyword . '%');
         }
 
-        $restaurants = $restaurants->paginate(5);
+        $restaurants = $restaurants->paginate(4);
         $types = Type::all();
 
         return response()->json(compact('restaurants', 'types'));
