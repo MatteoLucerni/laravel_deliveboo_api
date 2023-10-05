26b314f6bbba778c82581af5ba50253122cc6a86
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-05 12:26:59 +0200

    importing script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import "./delete-confirm";
+import "./restore-confirm";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
