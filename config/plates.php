58b8d802c0dbc07502307e7b46f9b82670660781
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-05 14:37:59 +0200

    fix: changed images paths

--- config/plates.php
+++ config/plates.php
@@ -3,7 +3,7 @@ return [
     [
         "name" => "Pizza Margherita",
         "price" => 10.99,
-        "image" => 'https://primochef.it/wp-content/uploads/2019/08/SH_pizza_fatta_in_casa-1200x800.jpg.webp',
+        "image" => 'plate_images/plate-1.jpg',
         "ingredients" => "Pomodoro, mozzarella, basilico",
         "description" => "La classica pizza Margherita con pomodoro fresco, mozzarella di bufala e basilico.",
         "is_visible" => true
@@ -11,7 +11,7 @@ return [
     [
         "name" => "Pasta alla Carbonara",
         "price" => 12.99,
-        "image" => "https://www.identitagolose.it/public/images/xmedium/pizza-2.jpg",
+        "image" => "plate_images/plate-1.jpg",
         "ingredients" => "Spaghetti, uova, pancetta, pecorino",
         "description" => "Una deliziosa pasta alla carbonara con spaghetti cotti al dente, uova fresche, pancetta croccante e pecorino romano.",
         "is_visible" => true
@@ -19,7 +19,7 @@ return [
     [
         "name" => "Sushi Misto",
         "price" => 18.99,
-        "image" => 'https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/10/tipologie-sushi-non-giapponesi.jpg',
+        "image" => 'plate_images/plate-2.jpg',
         "ingredients" => "Riso, pesce crudo, alga nori, avocado",
         "description" => "Un assortimento di sushi fresco con pesce crudo, avocado e riso appena preparato.",
         "is_visible" => true
@@ -27,7 +27,7 @@ return [
     [
         "name" => "Bistecca alla Griglia",
         "price" => 24.99,
-        "image" => "https://www.identitagolose.it/public/images/xmedium/pizza-2.jpg",
+        "image" => "plate_images/plate-3.jpg",
         "ingredients" => "Bistecca di manzo, sale, pepe, olio d'oliva",
         "description" => "Una succulenta bistecca alla griglia servita con sale, pepe e olio d'oliva.",
         "is_visible" => true
@@ -35,7 +35,7 @@ return [
     [
         "name" => "Insalata Caprese",
         "price" => 8.99,
-        "image" => 'https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/10/tipologie-sushi-non-giapponesi.jpg',
+        "image" => 'plate_images/plate-2.jpg',
         "ingredients" => "Pomodoro, mozzarella di bufala, basilico, olio d'oliva",
         "description" => "Un'insalata fresca e leggera con pomodoro, mozzarella di bufala, basilico e olio d'oliva extravergine.",
         "is_visible" => true
@@ -43,7 +43,7 @@ return [
     [
         "name" => "Lasagna alla Bolognese",
         "price" => 14.99,
-        "image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/10/tipologie-sushi-non-giapponesi.jpg",
+        "image" => "plate_images/plate-2.jpg",
         "ingredients" => "Fogli di pasta, carne macinata, besciamella, parmigiano",
         "description" => "Una deliziosa lasagna alla bolognese con strati di pasta, carne macinata, besciamella e parmigiano.",
         "is_visible" => true
@@ -51,7 +51,7 @@ return [
     [
         "name" => "Sushi Vegano",
         "price" => 16.99,
-        "image" => null,
+        "image" => 'placeholder.jpg',
         "ingredients" => "Riso, verdure fresche, alga nori",
         "description" => "Un assortimento di sushi vegano con verdure fresche, riso e alga nori.",
         "is_visible" => true
@@ -59,7 +59,7 @@ return [
     [
         "name" => "Tagliata di Manzo",
         "price" => 26.99,
-        "image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/10/tipologie-sushi-non-giapponesi.jpg",
+        "image" => "plate_images/plate-2.jpg",
         "ingredients" => "Bistecca di manzo, rucola, pomodorini, aceto balsamico",
         "description" => "Una succulenta tagliata di manzo servita con rucola, pomodorini e aceto balsamico.",
         "is_visible" => true
@@ -67,7 +67,7 @@ return [
     [
         "name" => "Insalata di Quinoa",
         "price" => 9.99,
-        "image" => null,
+        "image" => 'placeholder.jpg',
         "ingredients" => "Quinoa, verdure, feta, olive nere",
         "description" => "Un'insalata fresca e nutriente con quinoa, verdure miste, formaggio feta e olive nere.",
         "is_visible" => true
@@ -75,7 +75,7 @@ return [
     [
         "name" => "Tiramisù",
         "price" => 6.99,
-        "image" => "https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2022/10/tipologie-sushi-non-giapponesi.jpg",
+        "image" => "plate_images/plate-2.jpg",
         "ingredients" => "Savoiardi, mascarpone, caffè, cacao",
         "description" => "Un classico tiramisù italiano con savoiardi inzuppati nel caffè, mascarpone e cacao in polvere.",
         "is_visible" => true
