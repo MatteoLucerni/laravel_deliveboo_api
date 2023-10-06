d5506a03d46c07001f08f78d9a116b6d6a23a15a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-06 23:15:33 +0200

    show responsive

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+    <div class="jumbotron px-5 m-4 bg-light rounded-3">
         <div class="container py-5">
-            <div class="logo_laravel">
-            </div>
             <h1>Plate details</h1>
         </div>
     </div>
 
     <div class="content container">
-        <a href="{{ route('admin.plates.index') }}" class="btn btn-secondary">Go back to plates list</a>
         <div class="d-flex justify-content-center align-items-center text-center">
-            <div class="card my-4" style="width: 35rem;">
-                <img class="img-fluid rounded-1" src="{{ asset('storage/' . $plate->image) }}" alt="{{ $plate->name }}">
+            <div class="restaurant-card my-3 p-4" style="width: 35rem;">
+                <img class="img-fluid rounded-3 mb-3" src="{{ asset('storage/' . $plate->image) }}" alt="{{ $plate->name }}">
                 <div class="card-body">
                     <h3 class="card-title">{{ $plate->name }}</h3>
                     <div class="badge rounded-pill mb-1" style="background-color: {{ $plate->category->color }}">
@@ -21,16 +18,21 @@
                     <h5>{{ $plate->price }}€</h5>
                     <p class="card-text">{{ $plate->description }}</p>
                     <p><Strong>Ingredients: </Strong>{{ $plate->ingredients }}</p>
-                    <a class="btn btn-warning" href="{{ route('admin.plates.edit', $plate->id) }}">Edit</a>
-                    <form class="d-inline-block" action="{{ route('admin.plates.destroy', $plate) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger">
-                            <i class="fas fa-trash me-2"></i>Delete plate
-                        </button>
-                    </form>
+                    <div class="d-flex align-items-center justify-content-center gap-3">
+                        <a class="btn button-warning-db" href="{{ route('admin.plates.edit', $plate->id) }}"><i class="fa-solid fa-pen"></i> Edit</a>
+                        <form class="d-inline-block" action="{{ route('admin.plates.destroy', $plate) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn button-danger-db">
+                                <i class="fas fa-trash me-2"></i>Delete plate
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="d-flex justify-content-center mb-5">
+            <a href="{{ route('admin.plates.index') }}" class="btn button-secondary-db">Go back to plates list</a>
         </div>
     </div>
 @endsection
