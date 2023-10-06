8b550b9166e6a1dfb51cd11bf71a46186049a24a
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-10-06 11:24:19 +0200

    feat : restaurant seeder

--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -38,5 +38,38 @@ class PlateSeeder extends Seeder
         $food->description = "Una succulenta bistecca alla griglia servita con sale, pepe e olio d'oliva.";
         $food->is_visible = true;
         $food->save();
+
+        $food = new Plate();
+        $food->restaurant_id = 3;
+        $food->category_id = random_int(1, 5);
+        $food->name = 'Sushi Misto';
+        $food->price = 25.00;
+        $food->image = 'plate_images/plate-2.jpg';
+        $food->ingredients = 'Riso, salmone, tonno, avocado, alga nori, soia.';
+        $food->description = "Portata di sushi misto con i nostri migliori tagli.";
+        $food->is_visible = true;
+        $food->save();
+
+        $food = new Plate();
+        $food->restaurant_id = 4;
+        $food->category_id = random_int(1, 5);
+        $food->name = 'Chili Tacos';
+        $food->price = 14.00;
+        $food->image = 'plate_images/plate-4.jpg';
+        $food->ingredients = 'Tacos di farina di mais, carne di manzo, fagioli rossi, peperoncino.';
+        $food->description = "Un ottimo tacos, con dell'ottimo chili, e una buonissima salsa piccante.";
+        $food->is_visible = true;
+        $food->save();
+        $food = new Plate();
+
+        $food->restaurant_id = 5;
+        $food->category_id = random_int(1, 5);
+        $food->name = 'Doppio Cheese Burger';
+        $food->price = 7.99;
+        $food->image = 'plate_images/plate-5.jpg';
+        $food->ingredients = 'Hamburger di manzo, insalata, pomodoro, formaggio cheddar.';
+        $food->description = "Il grande classico!";
+        $food->is_visible = true;
+        $food->save();
     }
 }
