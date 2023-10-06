4c725997a404060511e6cb3a243a5b626955b6ac
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-06 13:30:14 +0200

    fix: migrate order

--- database/migrations/2023_09_27_085201_create_orders_table.php
+++ database/migrations/2023_09_27_085201_create_orders_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->date('order_date');
             $table->string('name');
             $table->string('surname');
             $table->char('tel', 10);
