c097bdb97c0d5945018ae257b10f7ba6f706dd9a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-10 13:06:41 +0200

    fixed show

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -1,33 +1,38 @@
 @extends('layouts.app')
 @section('content')
-    <div class="jumbotron px-5 m-4 bg-light rounded-3">
-        <div class="container py-5">
-            <h1>Plate details</h1>
-        </div>
-    </div>
+    <video autoplay muted preload="auto" class="object-fit-contain">
+        <source src="{{ asset('plate-details.mp4') }}" type="video/mp4">
+    </video>
 
     <div class="content container">
         <div class="d-flex justify-content-center align-items-center text-center">
-            <div class="restaurant-card my-3 p-4" style="width: 35rem;">
-                <img class="img-fluid rounded-3 mb-3" src="{{ asset('storage/' . $plate->image) }}" alt="{{ $plate->name }}">
-                <div class="card-body">
-                    <h3 class="card-title">{{ $plate->name }}</h3>
-                    <div class="badge rounded-pill mb-1" style="background-color: {{ $plate->category->color }}">
-                        {{ $plate->category->name }}
+            <div class="restaurant-card my-3 p-4 " style="width: 35rem;">
+                <div class="d-flex flex-column flex-lg-row">
+
+                    <div>
+                        <img class="img-fluid rounded-3 mb-3" src="{{ asset('storage/' . $plate->image) }}"
+                            alt="{{ $plate->name }}">
                     </div>
-                    <h5>{{ $plate->price }}€</h5>
-                    <p class="card-text">{{ $plate->description }}</p>
-                    <p><Strong>Ingredients: </Strong>{{ $plate->ingredients }}</p>
-                    <div class="d-flex align-items-center justify-content-center gap-3">
-                        <a class="btn button-warning-db" href="{{ route('admin.plates.edit', $plate->id) }}"><i class="fa-solid fa-pen"></i> Edit</a>
-                        <form class="d-inline-block" action="{{ route('admin.plates.destroy', $plate) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn button-danger-db">
-                                <i class="fas fa-trash me-2"></i>Delete plate
-                            </button>
-                        </form>
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $plate->name }}</h3>
+                        <div class="badge rounded-pill mb-1" style="background-color: {{ $plate->category->color }}">
+                            {{ $plate->category->name }}
+                        </div>
+                        <h5>{{ $plate->price }}€</h5>
+                        <p class="card-text">{{ $plate->description }}</p>
+                        <p><Strong>Ingredients: </Strong>{{ $plate->ingredients }}</p>
                     </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center gap-3 mt-4">
+                    <a class="btn button-warning-db" href="{{ route('admin.plates.edit', $plate->id) }}"><i
+                            class="fa-solid fa-pen"></i> Edit</a>
+                    <form class="d-inline-block" action="{{ route('admin.plates.destroy', $plate) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn button-danger-db">
+                            <i class="fas fa-trash me-2"></i>Delete plate
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
