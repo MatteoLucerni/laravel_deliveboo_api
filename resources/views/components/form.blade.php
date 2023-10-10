4ccec32be0b7e6a28f7a0deb3a01c6969a2fae81
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-10 12:41:10 +0200

    format document

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-        <div>
-                @if ($plate->exists)
-                <video autoplay muted preload="auto" class="object-fit-contain">
-                    <source src="{{ asset('edit-plate.mp4') }}" type="video/mp4">
-                </video>
-                @else
-                <video autoplay muted preload="auto" class="object-fit-contain">
-                    <source src="{{ asset('create-plate.mp4') }}" type="video/mp4">
-                </video>
-                @endif
-        </div>
+    <div>
+        @if ($plate->exists)
+            <video autoplay muted preload="auto" class="object-fit-contain">
+                <source src="{{ asset('edit-plate.mp4') }}" type="video/mp4">
+            </video>
+        @else
+            <video autoplay muted preload="auto" class="object-fit-contain">
+                <source src="{{ asset('create-plate.mp4') }}" type="video/mp4">
+            </video>
+        @endif
+    </div>
 
     <div class="content">
         <div class="container">
@@ -34,103 +34,103 @@
                         <input name="name" type="text" class="@error('name') is-invalid @enderror form-control"
                             id="plateName" value="{{ old('name', $plate->name) }}" required>
                         <small id="plate-name-error" class="text-danger"></small>
-                    @error('name')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <!-- Plate Image -->
+                    <div class="row">
+                        <div class="mb-3 col-12 col-lg-11">
+                            <label for="plateImage" class="form-label">Plate Image URL</label>
+                            <input name="image" type="file" class="@error('image') is-invalid @enderror form-control"
+                                id="plateImage">
+                            <input type="hidden" name="current_image" value="{{ $plate->image }}">
+                            @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
-                    @enderror
-                </div>
-
-                <!-- Plate Image -->
-                <div class="row">
-                    <div class="mb-3 col-12 col-lg-11">
-                        <label for="plateImage" class="form-label">Plate Image URL</label>
-                        <input name="image" type="file" class="@error('image') is-invalid @enderror form-control"
-                            id="plateImage">
-                        <input type="hidden" name="current_image" value="{{ $plate->image }}">
-                        @error('image')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="mb-3 col-1 d-none d-md-block d-lg-block">
-                        @if (!$plate->image)
-                            <img src="{{ asset('storage/placeholder.jpg') }}" class="img-fluid"
-                                style="object-fit: cover ; height:85px ; width:85px" id="preview">
-                        @elseif($plate->image)
-                            <img src="{{ asset('storage/' . $plate->image) }}" class="img-fluid"
-                                style="object-fit: cover ; height:85px ; width:85px" id="preview">
-                        @endif
-                    </div>
-
-                    <!-- Plate Price -->
-                    <div class="mb-3 me-3">
-                        <label for="platePrice" class="form-label">Plate Price <sup class="text-danger">*</sup></label>
-                        <input name="price" type="number" class="@error('price') is-invalid @enderror form-control"
-                            id="platePrice" value="{{ old('price', $plate->price) }}" required>
-                        <small id="plate-price-error" class="text-danger"></small>
-                        @error('price')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <!-- Plate Ingredients -->
-                    <div class="mb-3">
-                        <label for="plateIngredients" class="form-label">Plate Ingredients <sup
-                                class="text-danger">*</sup></label>
-                        <textarea name="ingredients" class="@error('ingredients') is-invalid @enderror form-control" id="plateIngredients"
-                            required>{{ old('ingredients', $plate->ingredients) }}</textarea>
-                        <small id="plate-ingredients-error" class="text-danger"></small>
-                        @error('ingredients')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <!-- Plate Description -->
-                    <div class="mb-3">
-                        <label for="plateDescription" class="form-label">Plate Description</label>
-                        <textarea name="description" class="@error('description') is-invalid @enderror form-control" id="plateDescription">{{ old('description', $plate->description) }}</textarea>
-                        @error('description')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <!-- Plate visibility -->
-                    <div class="mb-3">
-                        <label for="is_visible" class="form-label">Visible?</label>
-                        <input value="1" type="checkbox" name="is_visible" id="is_visible"
-                            {{ old('is_visible', $plate->is_visible) ? 'checked' : '' }}>
-                    </div>
-
-                    <!-- Plate Category -->
-                    <div class="mb-3">
-                        <label for="is_visible" class="form-label">Category <sup class="text-danger">*</sup></label>
-                        <select name="category_id" class="form-select form-select-lg mb-3"
-                            aria-label="Large select example">
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <!-- Submit Button -->
-                    <div class="d-flex align-items-center justify-content-center justify-content-lg-start">
-
-                        <button type="submit" class="button-main-db me-3" id="submit-button">
-                            @if ($plate->exists)
-                            Update
-                            @else
-                            Create
+                        <div class="mb-3 col-1 d-none d-md-block d-lg-block">
+                            @if (!$plate->image)
+                                <img src="{{ asset('storage/placeholder.jpg') }}" class="img-fluid"
+                                    style="object-fit: cover ; height:85px ; width:85px" id="preview">
+                            @elseif($plate->image)
+                                <img src="{{ asset('storage/' . $plate->image) }}" class="img-fluid"
+                                    style="object-fit: cover ; height:85px ; width:85px" id="preview">
                             @endif
-                        </button>
-                        <a class="btn button-secondary-db" href="{{ route('admin.plates.index') }}">Go back</a>
-                    </div>
+                        </div>
+
+                        <!-- Plate Price -->
+                        <div class="mb-3 me-3">
+                            <label for="platePrice" class="form-label">Plate Price <sup class="text-danger">*</sup></label>
+                            <input name="price" type="number" class="@error('price') is-invalid @enderror form-control"
+                                id="platePrice" value="{{ old('price', $plate->price) }}" required>
+                            <small id="plate-price-error" class="text-danger"></small>
+                            @error('price')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <!-- Plate Ingredients -->
+                        <div class="mb-3">
+                            <label for="plateIngredients" class="form-label">Plate Ingredients <sup
+                                    class="text-danger">*</sup></label>
+                            <textarea name="ingredients" class="@error('ingredients') is-invalid @enderror form-control" id="plateIngredients"
+                                required>{{ old('ingredients', $plate->ingredients) }}</textarea>
+                            <small id="plate-ingredients-error" class="text-danger"></small>
+                            @error('ingredients')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <!-- Plate Description -->
+                        <div class="mb-3">
+                            <label for="plateDescription" class="form-label">Plate Description</label>
+                            <textarea name="description" class="@error('description') is-invalid @enderror form-control" id="plateDescription">{{ old('description', $plate->description) }}</textarea>
+                            @error('description')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <!-- Plate visibility -->
+                        <div class="mb-3">
+                            <label for="is_visible" class="form-label">Visible?</label>
+                            <input value="1" type="checkbox" name="is_visible" id="is_visible"
+                                {{ old('is_visible', $plate->is_visible) ? 'checked' : '' }}>
+                        </div>
+
+                        <!-- Plate Category -->
+                        <div class="mb-3">
+                            <label for="is_visible" class="form-label">Category <sup class="text-danger">*</sup></label>
+                            <select name="category_id" class="form-select form-select-lg mb-3"
+                                aria-label="Large select example">
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <!-- Submit Button -->
+                        <div class="d-flex align-items-center justify-content-center justify-content-lg-start">
+
+                            <button type="submit" class="button-main-db me-3" id="submit-button">
+                                @if ($plate->exists)
+                                    Update
+                                @else
+                                    Create
+                                @endif
+                            </button>
+                            <a class="btn button-secondary-db" href="{{ route('admin.plates.index') }}">Go back</a>
+                        </div>
                 </form>
             </div>
         </div>
