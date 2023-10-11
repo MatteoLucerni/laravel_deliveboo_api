f6b98cb0254793da5a62e191d14410da000964c9
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-11 12:42:59 +0200

    feat: added structure of the toast

--- resources/views/includes/toast.blade.php
+++ resources/views/includes/toast.blade.php
@@ -1,11 +1,15 @@
-<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-        <img src="..." class="rounded me-2" alt="...">
-        <strong class="me-auto">DeliveBoo</strong>
-        <small>Just now</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+@if (session('toast-message'))
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast show bg-orange-light" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-orange-strong">
+                <img class="toast-image me-2" src="{{ asset('Yummy_Food.svg') }}" class="rounded me-2" alt="...">
+                <strong class="me-auto">DeliveBoo</strong>
+                <small>Just Now</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{ session('toast-message') }}
+            </div>
+        </div>
     </div>
-    <div class="toast-body">
-        Hello, world! This is a toast message.
-    </div>
-</div>
+@endif
