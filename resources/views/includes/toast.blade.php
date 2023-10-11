ef6489e39edd20008e4870a5b61a6b08aa94aeed
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-11 11:50:25 +0200

    feat: added toast include comp

---
+++ resources/views/includes/toast.blade.php
@@ -0,0 +1,11 @@
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+        <img src="..." class="rounded me-2" alt="...">
+        <strong class="me-auto">DeliveBoo</strong>
+        <small>Just now</small>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+        Hello, world! This is a toast message.
+    </div>
+</div>
